14d3f1c615d9551857f081b31d2bb0f9dbb246c0
evgen <benkliii96@gmail.com>
2026-03-31 12:40:22 +0300

    pizda

--- backend/dist/progress/progress.module.js
+++ backend/dist/progress/progress.module.js
@@ -10,15 +10,11 @@ exports.ProgressModule = void 0;
 const common_1 = require("@nestjs/common");
 const progress_service_1 = require("./progress.service");
 const progress_controller_1 = require("./progress.controller");
-const topics_module_1 = require("../topics/topics.module");
-const submissions_module_1 = require("../submissions/submissions.module");
-const users_module_1 = require("../users/users.module");
 let ProgressModule = class ProgressModule {
 };
 exports.ProgressModule = ProgressModule;
 exports.ProgressModule = ProgressModule = __decorate([
     (0, common_1.Module)({
-        imports: [topics_module_1.TopicsModule, submissions_module_1.SubmissionsModule, users_module_1.UsersModule],
         providers: [progress_service_1.ProgressService],
         controllers: [progress_controller_1.ProgressController],
         exports: [progress_service_1.ProgressService],
--- backend/src/progress/progress.module.ts
+++ backend/src/progress/progress.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ProgressService } from './progress.service';
 import { ProgressController } from './progress.controller';
-import { TopicsModule } from '../topics/topics.module';
-import { SubmissionsModule } from '../submissions/submissions.module';
-import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TopicsModule, SubmissionsModule, UsersModule],
   providers: [ProgressService],
   controllers: [ProgressController],
   exports: [ProgressService],
